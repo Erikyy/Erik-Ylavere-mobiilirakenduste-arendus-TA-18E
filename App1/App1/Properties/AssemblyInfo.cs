1cee0c4434153acd1a4f09054a57feadb2b7b7eb
Erik Ylavere <erik.ylavere@tptlive.ee>
2019-09-05 08:59:49 +0300

    second commit

--- App1/App1/Properties/AssemblyInfo.cs
+++ App1/App1/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mobiili_rakendus_Erik_Ylavere")]
+[assembly: AssemblyTitle("App1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Mobiili_rakendus_Erik_Ylavere")]
+[assembly: AssemblyProduct("App1")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
