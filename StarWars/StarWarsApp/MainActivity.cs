edd348ad574feebca0194a319e439724ca3ae0f7
Erik Ylavere <erik.ylavere@tptlive.ee>
2019-12-10 14:34:54 +0200

    Finished

---
+++ StarWars/StarWarsApp/MainActivity.cs
@@ -0,0 +1,63 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using StarWarsApp.Core;
+using System;
+using Android.Content;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+
+namespace StarWarsApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            AppCenter.Start("8bd811f0-e45f-4d87-9dc1-c38770c9249a",
+                   typeof(Analytics), typeof(Crashes));
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            //var imageView = FindViewById<ImageView>(Resource.Id.mainImageView);
+            //var drawable = (int)typeof(Resource.Drawable).GetField("Luke_skywalker.jpg").GetValue(null);
+            var toPeopleBtn = FindViewById<Button>(Resource.Id.toPeopleButton);
+            var toPlanetsBtn = FindViewById<Button>(Resource.Id.toPlanetsButton);
+            var toStarshipsBtn = FindViewById<Button>(Resource.Id.toStarshipsButton);
+            var toMoviesListBtn = FindViewById<Button>(Resource.Id.toMoviesListButton);
+
+            toPeopleBtn.Click += ToPeopleBtn_Click;
+            toPlanetsBtn.Click += ToPlanetsBtn_Click;
+            toStarshipsBtn.Click += ToStarshipsBtn_Click;
+            toMoviesListBtn.Click += ToMoviesListBtn_Click;
+
+        }
+
+        private void ToPeopleBtn_Click(object sender, EventArgs e)
+        {
+            var intent = new Intent(this, typeof(People_Activity));
+            this.StartActivity(intent);
+        }
+        private void ToPlanetsBtn_Click(object sender, EventArgs e)
+        {
+            var intent = new Intent(this, typeof(Planets_Activity));
+            this.StartActivity(intent);
+        }
+        private void ToStarshipsBtn_Click(object sender, EventArgs e)
+        {
+            var intent = new Intent(this, typeof(Starships_Activity));
+            this.StartActivity(intent);
+        }
+        private void ToMoviesListBtn_Click(object sender, EventArgs e)
+        {
+            var intent = new Intent(this, typeof(Movies_Activity));
+            this.StartActivity(intent);
+        }
+    }
+}
---
+++ StarWars/StarWarsApp/Movies_Activity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using StarWarsApp.Core;
+using Newtonsoft.Json;
+using static Android.Widget.AdapterView;
+
+namespace StarWarsApp
+{
+    [Activity(Label = "Movies_Activity")]
+    public class Movies_Activity : Activity
+    {
+        protected async override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Movies_Layout);
+
+            var moviesListView = FindViewById<ListView>(Resource.Id.moviesListView);
+
+            string queryString = "https://swapi.co/api/films/";
+            var data = await DataServiceMovies.GetStarWarsMovies(queryString);
+            moviesListView.Adapter = new StarWarsMoviesAdapter(this, data.Results);
+
+            moviesListView.ItemClick += (object sender, ItemClickEventArgs e) =>
+            {
+                //var clickPostitionText = moviesListView.GetItemAtPosition(e.Position); // Show text
+                //var clickPostitionID = Convert.ToString(e.Position); // Show index
+                var movieDetails = data.Results[e.Position];
+
+                var intent = new Intent(this, typeof(MovieDetailsActivity));
+                intent.PutExtra("movieDetails", JsonConvert.SerializeObject(movieDetails));
+                StartActivity(intent);
+            };
+        }
+    }
+}
